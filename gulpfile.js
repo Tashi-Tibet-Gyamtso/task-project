51d643f167b1a9087117cf990807e3d6d0dbe287
Tashi Tibet Gyamtso <tashigyamtso@hotmail.com>
2019-03-21 02:11:29 +0100

    task-project

---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+'use strict';
+ 
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var uglifycss = require('gulp-uglifycss');
+ 
+sass.compiler = require('node-sass');
+ 
+gulp.task('sass', function () {
+  return gulp.src('./scss/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./css'));
+});
+
+  gulp.task('css', function () {
+    gulp.src('./css/*.css')
+      .pipe(uglifycss({
+        "maxLineLen": 80,
+        "uglyComments": true
+      }))
+      .pipe(gulp.dest('./dist/'));
+  });
+
+gulp.task('sass:watch', function () {
+    gulp.watch('./scss/*.scss', ['sass']);
+    gulp.watch('./css/*.css', ['css']);
+  });
